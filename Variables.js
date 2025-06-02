d58ad66056d0b98c8a7c329a86f0f44369b16a4c
Anuj Kumar <anuj20792@gmail.com>
2025-06-02 15:48:25 +0530

    first commit

---
+++ Variables.js
@@ -0,0 +1,27 @@
+/*
+Variables क्या होते हैं? Variables एक container की तरह होते हैं जो डेटा को store करते हैं। 
+JavaScript में variables declare करने के 3 तरीके हैं:
+
+1. var
+2. let
+3. const
+
+Variables का उपयोग डेटा को store करने, manipulate करने और access करने के लिए किया जाता है।
+*/
+
+
+// var का उदाहरण
+
+var name = "Anuj";
+var age = 25;
+
+// let का उदाहरण
+
+let city = "Kanpur";
+let isStudent = true;
+
+// const का उदाहरण
+
+const PI = 3.14;
+const BirthYear = 1998;
+
