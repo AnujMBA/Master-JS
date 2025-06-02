4c898d7601886ef356cab64af6bb613c1692bcc4
Anuj Kumar <anuj20792@gmail.com>
2025-06-02 16:07:23 +0530

    variable types

--- Variables.js
+++ Variables.js
@@ -25,3 +25,55 @@ let isStudent = true;
 const PI = 3.14;
 const BirthYear = 1998;
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Variables के प्रकार // 
+
+/* 
+1. Number
+2. String
+3. Boolean
+4. Object
+5. Array
+6. Null
+7. Undefined
+8. Symbol
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Variables के प्रकार का उदाहरण
+
+let marks = 95;
+let percentage = 85.5;
+
+let firstName = "Anuj";
+let message = "Hello, World!";
+
+let isPass = true;
+let isFall = false;
+
+let colors = ["Red", "Green", "Blue"];
+let numbers = [1, 2, 3, 4, 5];
+
+let student = {
+    name: "Anuj",
+    age: 25,
+    city: "Kanpur"
+}
+
+let kuchBhi;
+//console.log(kuchBhi); // Undefined
+
+let nothing = null; // Null value
+
+let symbolobject = Symbol("ID"); // Symbol type
+
+let badiValue = 12345678901234567890n; // BigInt type
+
+// Variables के उपयोग का उदाहरण
+    
+
+console.log(symbolobject);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
